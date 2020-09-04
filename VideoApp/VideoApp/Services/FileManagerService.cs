e50fdc5e855c820ef2e85622b2b7950142e30000
Mihai Lita <mihai.lita@easydo247.com>
2020-09-04 21:44:28 +0300

    Renamed some methods in the controller Renamed the controller route Renamed a DTO to be used in another method. Added method that downloads a file from file name.

--- VideoApp/VideoApp/Services/FileManagerService.cs
+++ VideoApp/VideoApp/Services/FileManagerService.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using VideoApp.Web.Models.ViewModels;
 
 namespace VideoApp.Web.Services
 {
@@ -29,7 +31,7 @@ namespace VideoApp.Web.Services
             {
                 throw new Exception("Error deleteing TEMP file: " + ex.Message);
             }
-        }
+        }     
 
         public async Task<string> SaveTempFile(IFormFile file, string fileName)
         {
@@ -47,6 +49,53 @@ namespace VideoApp.Web.Services
             {
                 throw new Exception("file must not be empty");
             }
+        }
+
+        public async Task<OutputFileModel> GetFile(string fileName)
+        {
+            var filePath = Path.Combine(_hostingEnvironment.ContentRootPath, "Uploads", fileName);
+            var contentType = GetContentType(fileName);
+            var fileStream = ReadFile(filePath);
+            var outputFile = new OutputFileModel
+            {
+                FileName = fileName,
+                FileContents = fileStream,
+                ContentType = contentType
+            };
+            return await Task.FromResult(outputFile);
+        }
+
+        private byte[] ReadFile(string filePath)
+        {
+            byte[] buffer;
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            try
+            {
+                int length = (int)fileStream.Length;  // get file length
+                buffer = new byte[length];            // create buffer
+                int count;                            // actual number of bytes read
+                int sum = 0;                          // total number of bytes read
+
+                // read until Read method returns 0 (end of the stream has been reached)
+                while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+                    sum += count;  // sum is a buffer offset for next reading
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+            return buffer;
+        }
+
+        private string GetContentType(string filename)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(filename, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
         }
     }
 }
--- video-converter-app/src/app/video-detail/video-detail.component.ts
+++ video-converter-app/src/app/video-detail/video-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { VideoService } from '../video.service';
 import { Video } from '../models/video';
+import { OutputFormat } from '../models/outputFormat';
+import * as fileSaver from 'file-saver';
 
 @Component({
   selector: 'app-video-detail',
@@ -9,11 +12,66 @@ import { Video } from '../models/video';
 })
 export class VideoDetailComponent implements OnInit {
   id;
-  constructor(private _Activatedroute: ActivatedRoute) {}
+  selectedValue: string;
+  video: Video;
+  isLoading = true;
+
+  formats: OutputFormat[] = [
+    { value: 'Hd480', viewValue: 'HD 480p' },
+    { value: 'Hd720', viewValue: 'HD 720p' },
+    { value: 'Hd1080', viewValue: 'HD 1080p' },
+  ];
+  constructor(
+    private _Activatedroute: ActivatedRoute,
+    private videoService: VideoService
+  ) {}
 
   ngOnInit(): void {
     this._Activatedroute.paramMap.subscribe((params) => {
       this.id = params.get('id');
+      this.getVideos(this.id);
     });
+  }
+
+  getVideos(id: number) {
+    this.videoService.getVideoById(id).subscribe(
+      (video) => {
+        this.video = video;
+        this.isLoading = false;
+        console.log(this.video);
+      },
+      (error) => {
+        this.isLoading = false;
+        console.log(error);
+      }
+    );
+  }
+
+  generateHLS() {
+    console.log('generate HLS');
+  }
+
+  getThumbnails() {
+    console.log('getting thumbnails');
+  }
+
+  convertToOutput() {
+    console.log('gconvert to output');
+  }
+
+  download(filename) {
+    this.isLoading = true;
+    console.log('download clicked', filename);
+    this.videoService.downloadFile(filename).subscribe((response) => {
+      console.log(response);
+      this.isLoading = false;
+      let blob: any = new Blob([response.fileContents], {
+        type: response.contentType,
+      });
+      // const url = window.URL.createObjectURL(blob);
+      fileSaver.saveAs(blob, response.fileName);
+    }),
+      (error) => console.log('Error downloading the file'),
+      () => console.info('File downloaded successfully');
   }
 }
